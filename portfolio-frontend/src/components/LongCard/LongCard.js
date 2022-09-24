1a25b5f718f0932271224825ce878ebc73d9bc04
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2022-09-24 23:58:35 +0200

    Add swiper.js implementation of the pictures on the projects

--- portfolio-frontend/src/components/LongCard/LongCard.js
+++ portfolio-frontend/src/components/LongCard/LongCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
 import { getChipColor } from '../../utils';
 import Chip from '../Chip/Chip';
+import { EffectCards } from 'swiper';
 
 function LongCard({
-	thumbnail,
+	image_1,
+	image_2,
+	image_3,
 	chips,
 	title,
 	title_bg,
@@ -16,11 +22,35 @@ function LongCard({
 	return (
 		<a href={url} target="_blank" rel="noreferrer">
 			<article className="flex flex-wrap sm:flex-nowrap mb-10">
-				<img
-					className="h-48 w-48 rounded border border-slate-300 object-cover"
-					src={thumbnail}
-					alt="Post thumbnail"
-				/>
+				<Swiper
+					effect={'cards'}
+					grabCursor={true}
+					modules={[EffectCards]}
+					className="h-48 w-48"
+				>
+					<SwiperSlide>
+						<img
+							className="object-cover h-48 w-48 rounded border border-slate-300"
+							src={image_1}
+							alt="Post thumbnail"
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img
+							className="object-cover h-48 w-48 rounded border border-slate-300"
+							src={image_2}
+							alt="Post thumbnail"
+						/>
+					</SwiperSlide>
+					<SwiperSlide>
+						<img
+							className="object-cover h-48 w-48 rounded border border-slate-300"
+							src={image_3}
+							alt="Post thumbnail"
+						/>
+					</SwiperSlide>
+				</Swiper>
+
 				<div className="ml-0 sm:ml-12">
 					<h3 className="text-2xl font-bold mb-2">
 						{i18n.language === 'en' ? title : title_bg}
